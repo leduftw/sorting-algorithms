a17bd360954b41fec9cf032aabea8a47fc11f479
Dusko Sretenovic <dsretenovic555@gmail.com>
2020-09-30 13:36:33 +0200

    Add Knuth gap sequence strategy

---
+++ Sorting/knuth_gap_strategy.cpp
@@ -0,0 +1,14 @@
+#include "knuth_gap_strategy.h"
+
+
+vector<int> KnuthGapStrategy::getGaps(size_t n) const {
+	size_t k = log(2 * (n / 3) + 1) / log(3);
+	vector<int> ans(k);
+
+	ans[k - 1] = 1;  // ensuring valid sorting
+	for (int i = (int)k - 2; i >= 0; i--) {
+		ans[i] = 3 * ans[i + 1] + 1;
+	}
+
+	return ans;
+}
---
+++ Sorting/knuth_gap_strategy.h
@@ -0,0 +1,26 @@
+#ifndef KNUTH_GAP_STRATEGY
+#define KNUTH_GAP_STRATEGY
+
+#include <vector>
+#include "gap_strategy.h"
+
+using namespace std;
+
+class KnuthGapStrategy : public GapStrategy { 
+
+public:
+
+	/*
+		Gaps are of the form (3^k - 1) / 2, where k = 1, 2, 3,...
+		This can be expressed recursively: A(k) = 3*A(k-1) + 1.
+		Largest gap should not be greater than ceil(n / 3).
+
+		This means that largest valid gap is obtained when
+		k = floor(log3(2 * ceil(n / 3) + 1)), which is also
+		the size of returned result array.
+	*/
+	virtual vector<int> getGaps(size_t n) const override;
+
+};
+
+#endif
