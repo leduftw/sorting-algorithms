a77ee5a81953833217d3d9bc2353f0c7e8385972
Dusko Sretenovic <dsretenovic555@gmail.com>
2020-09-30 13:16:08 +0200

    Add abstract base class for gap sequence strategy

---
+++ Sorting/gap_strategy.h
@@ -0,0 +1,20 @@
+#ifndef GAP_STRATEGY
+#define GAP_STRATEGY
+
+#include <vector>
+
+using namespace std;
+
+class GapStrategy { 
+	
+public:
+
+	/*
+		Returns gap sequence in decreasing order.
+		Parameter n is size of array on which sorting is performed.
+	*/
+	virtual vector<int> getGaps(size_t n) const = 0;
+
+};
+
+#endif
